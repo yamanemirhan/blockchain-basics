12f02f71a337f5dcf5487def919b7c07177ff4c7
yamanemirhan <yaman.emrhn@gmail.com>
2024-07-08 03:21:25 +0300

    Fund Me

---
+++ examples/FundMe.sol
@@ -0,0 +1,18 @@
+    // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 50 * 1e18;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+}
