ad406e4dbbc73f0c3bc4bb5e62de3fc8eaf0cc54
yamanemirhan <yaman.emrhn@gmail.com>
2024-07-08 03:22:08 +0300

    Price Converter Library

---
+++ examples/PriceConverter.sol
@@ -0,0 +1,25 @@
+    // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+
+    function getPrice() internal view returns(uint256) {
+        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306 Sepolia:ETH/USD
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 answer, , ,) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+    function getVersion() internal view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
